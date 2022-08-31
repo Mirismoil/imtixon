a748587855dbc892a65024d295e6b1c6fba363d5
Mirismoil <yuldashevmirismoil3@gmail.com>
2022-08-31 20:33:24 +0500

    s

---
+++ main.js
@@ -0,0 +1,12 @@
+// 1-chi masala
+// let arr = ["I", "study", "javascript", "right", "now"];
+// arr.splice(2, 1);
+// console.log(arr);
+
+// 2-chi masala
+function hello(world) {
+  let world1 = world.split(" ");
+
+  return console.log(world1);
+}
+hello("salom dunyo");
